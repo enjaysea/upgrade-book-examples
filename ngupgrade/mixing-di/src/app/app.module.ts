d8c7d7801465eff3a7ec79ddb01f4f635c5d62f4
Centanni <ncentanni@fortinet.com>
2020-05-20 13:19:20 -0700

    Initial

--- ngupgrade/mixing-di/src/app/app.module.ts
+++ ngupgrade/mixing-di/src/app/app.module.ts
@@ -1,7 +1,6 @@
 import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
 
-import 'angular';
 import {downgradeInjectable, UpgradeModule} from '@angular/upgrade/static';
 
 declare const angular: any;
